6c38b8f83088846e1d05f6f5485ee4470f7b03fa
Zachary Guns <zacharyguns@yahoo.com>
2018-10-18 23:25:58 -0700

    expanded upon the hide/show example on the jquery card

--- card_pages/CN_jQuery/CN_jQuery.js
+++ card_pages/CN_jQuery/CN_jQuery.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+    $("#hideBtn").click(function(){
+        $("#hideshowP").hide();
+    });
+});
+
+$(document).ready(function(){
+    $("#showBtn").click(function(){
+        $("#hideshowP").show();
+    });
+});
+
+$(document).ready(function(){
     $("#toggleBtn").click(function(){
         $("#toggleP").toggle();
     });

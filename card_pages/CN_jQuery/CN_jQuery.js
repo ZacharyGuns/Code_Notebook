0245a361d56816dd95f7e38fa5c25bf4e5814220
Zachary Guns <zacharyguns@yahoo.com>
2018-10-20 22:55:51 -0700

    working on adding an Animate example with multiple animate command buttons

--- card_pages/CN_jQuery/CN_jQuery.js
+++ card_pages/CN_jQuery/CN_jQuery.js
@@ -27,11 +27,18 @@ $(document).ready(function(){
 // =================================================================================
 
 $(document).ready(function(){
-    $("#animateBtn").click(function(){
+    $("#animateBlue").click(function(){
         var div = $("#blueSquare");
         div.animate({height: '300px', opacity: '0.4'}, "slow");
         div.animate({width: '300px', opacity: '0.8'}, "slow");
         div.animate({height: '100px', opacity: '0.4'}, "slow");
         div.animate({width: '100px', opacity: '0.8'}, "slow");
+    });
+});
+
+$(document).ready(function(){
+    $("#animateRight").click(function(){
+        var div = $("#purpleSquare");
+        div.animate({left: '250px'});
     });
 });

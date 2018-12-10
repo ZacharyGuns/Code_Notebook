a4cfa3c9e3c9614e7f4319b3a6174248fef1f564
Zachary Guns <zacharyguns@yahoo.com>
2018-12-09 16:22:34 -0800

    beginning to add code for a Web Workers example

--- card_pages/CN_HTML/CN_HTML.js
+++ card_pages/CN_HTML/CN_HTML.js
@@ -11,3 +11,25 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
+
+// =============================================================================================================
+
+var w;
+
+function startWorker() {
+  if(typeof(Worker) !== "undefined") {
+    if(typeof(w) == "undefined") {
+      w = new Worker("demo_workers.js");
+    }
+    w.onmessage = function(event) {
+      document.getElementById("result").innerHTML = event.data;
+    };
+  } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Workers...";
+  }
+}
+
+function stopWorker() { 
+  w.terminate();
+  w = undefined;
+}

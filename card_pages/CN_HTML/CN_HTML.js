a453e9dfe5359d8152e9e185cd36c62ddc08352b
Zachary Guns <zacharyguns@yahoo.com>
2018-12-09 16:33:05 -0800

    deleted the Web Workers setup code and finished the examples for the Web Storage example

--- card_pages/CN_HTML/CN_HTML.js
+++ card_pages/CN_HTML/CN_HTML.js
@@ -12,24 +12,30 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));
 }
 
-// =============================================================================================================
+// ==========================================================================================================
 
-var w;
-
-function startWorker() {
-  if(typeof(Worker) !== "undefined") {
-    if(typeof(w) == "undefined") {
-      w = new Worker("demo_workers.js");
+function clickCounter1() {
+    if (typeof(Storage) !== "undefined") {
+      if (localStorage.clickcount) {
+        localStorage.clickcount = Number(localStorage.clickcount)+1;
+      } else {
+        localStorage.clickcount = 1;
+      }
+      document.getElementById("result1").innerHTML = "You have clicked the button " + localStorage.clickcount + " time(s).";
+    } else {
+      document.getElementById("result1").innerHTML = "Sorry, your browser does not support web storage...";
     }
-    w.onmessage = function(event) {
-      document.getElementById("result").innerHTML = event.data;
-    };
-  } else {
-    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Workers...";
   }
-}
 
-function stopWorker() { 
-  w.terminate();
-  w = undefined;
-}
+  function clickCounter2() {
+    if (typeof(Storage) !== "undefined") {
+      if (sessionStorage.clickcount) {
+        sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
+      } else {
+        sessionStorage.clickcount = 1;
+      }
+      document.getElementById("result2").innerHTML = "You have clicked the button " + sessionStorage.clickcount + " time(s) in this session.";
+    } else {
+      document.getElementById("result2").innerHTML = "Sorry, your browser does not support web storage...";
+    }
+  }

3427a354ec0d37df682c4f20323f9ee738aee7b3
Zachary Guns <zacharyguns@yahoo.com>
2019-01-10 23:27:11 -0800

    Filters example is working

--- card_pages/CN_Bootstrap/CN_Bootstrap.js
+++ card_pages/CN_Bootstrap/CN_Bootstrap.js
@@ -3,3 +3,12 @@ $(document).ready(function () {
 });
 
 // ===========================================================================================================
+
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});

a30e0093fda6377d2282404a19bef4e1701067da
Zachary Guns <zacharyguns@yahoo.com>
2019-01-28 22:44:54 -0800

    updated nav bar for all working pages

--- card_pages/CN_APIs/CN_APIs.js
+++ card_pages/CN_APIs/CN_APIs.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 
     var animals = [
-      "dog", "cat", "rabbit", "hamster", "skunk", "goldfish",
-      "bird", "ferret", "turtle", "sugar glider", "chinchilla",
-      "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken",
-      "capybara", "teacup pig", "serval", "salamander", "frog"
+    //   "dog", "cat", "rabbit", "hamster", "skunk", "goldfish",
+    //   "bird", "ferret", "turtle", "sugar glider", "chinchilla",
+    //   "hedgehog", "hermit crab", "gerbil", "pygmy goat", "chicken",
+    //   "capybara", "teacup pig", "serval", "salamander", "frog"
+        "zebra", "dinosaur", "league of legends", "overwatch", "pokemon"
     ];
   
     // function to make buttons and add to page

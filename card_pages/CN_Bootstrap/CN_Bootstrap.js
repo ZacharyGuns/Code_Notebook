90ffbbc02abd01a3f33c5e169733b5ac9b946fce
Zachary Guns <zacharyguns@yahoo.com>
2019-02-10 22:26:54 -0800

    added another example to the Filters example on the Bootstrap card

--- card_pages/CN_Bootstrap/CN_Bootstrap.js
+++ card_pages/CN_Bootstrap/CN_Bootstrap.js
@@ -12,3 +12,14 @@ $(document).ready(function(){
     });
   });
 });
+
+// ===========================================================================================================
+
+$(document).ready(function(){
+  $("#myInput2").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $(".dropdown-menu li").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
